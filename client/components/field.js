e07e402e6c8547cdffb6cd0f24100258bba88064
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-12-02 14:44:57 -0600

    WIP

--- client/components/field.js
+++ client/components/field.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Space } from './'
+
 import { updateBoard } from '../store'
+import { markSpaces } from '../script'
 import firebase from '../firebase'
 
 import '../css/field.scss';
@@ -11,8 +12,12 @@ class Field extends Component {
 
   componentDidMount(){
     const {getBoard, spaces} = this.props
-    getBoard()
-    markSpaces(spaces)
+    // getBoard()
+    // markSpaces(spaces)
+  }
+
+  componentWillReceiveProps(){
+    markSpaces(this.props.spaces)
   }
 
   render() {
@@ -22,7 +27,7 @@ class Field extends Component {
       <div className="field">
       {
         spaces && spaces.map(space => {
-          return (<div className="space" key={space.id} space={space} onClick={() => handleClick(space)} />)
+          return (<div className="space" key={space.id} id={`${space.coords[0]}, ${space.coords[1]}`} onClick={() => handleClick(space)} />)
         })
       }
       </div>
@@ -39,18 +44,19 @@ const mapState = (state) => {
 
 const mapDispatch = (dispatch, ownProps) => {
   const gameId = ownProps.match.params.gameId
-  // updateGame(dispatch)
-  return {
-    getBoard: () => {
-      firebase.ref(`/games/${gameId}/`).on('value', snap => {
-        dispatch(updateBoard(snap.val()))
-      })
-    },
+
+  firebase.ref(`/games/${gameId}/`).on('value', snap => {
+    dispatch(updateBoard(snap.val()))})
+
+    return {
+    // getBoard: () => {
+    //   firebase.ref(`/games/${gameId}/`).on('value', snap => {
+    //     dispatch(updateBoard(snap.val()))
+    //   })
+    // },
     handleClick: (space) => {
-      // if (!space.hasBall) {
-        firebase.ref(`/games/${gameId}/state/`).update({selectedSpace: space.id})
-        console.log(space)
-      // }
+      console.log('CLICK:', space)
+      firebase.ref(`/games/${gameId}/state/`).update({selectedSpace: space.id})
     }
   }
 }
---
+++ client/script/index.js
@@ -0,0 +1,6 @@
+export const markSpaces = spaces => {
+  console.log(spaces)
+  // spaces.map(space => {
+  //   if (space.hasBall) space.classList.add('hasBall')
+  // })
+}
