39f048f39aa374a2cd5534f2014a994fd5f2dc8b
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-12-03 11:49:16 -0600

    highlights current selected space and de-highlights past selected spaces

--- client/components/field.js
+++ client/components/field.js
@@ -11,27 +11,33 @@ import '../css/field.scss';
 class Field extends Component {
 
   componentDidMount(){
-    const {getBoard, spaces} = this.props
-    // getBoard()
+
   }
 
   componentWillReceiveProps(){
     const {state, spaces} = this.props
 
     if (state) {
-      markSpaces(spaces, state.selectedSpace)
+      // markSpaces(spaces, state.selectedSpace)
     }
   }
 
   render() {
-    const { spaces, handleClick } = this.props
-    // spaces && markSpaces(spaces)
+    const { spaces, handleClick, state } = this.props
 
     return (
       <div className="field">
       {
         spaces && spaces.map(space => {
-          return (<div className="space" key={space.id} id={`${space.coords[0]}, ${space.coords[1]}`} onClick={() => handleClick(space)} />)
+          return (<div
+            className="space"
+            key={space.id}
+            id={`${space.coords[0]}, ${space.coords[1]}`}
+            onClick={() => {
+              state &&
+              handleClick(space)
+            }}
+          />)
         })
       }
       </div>
@@ -59,9 +65,40 @@ const mapDispatch = (dispatch, ownProps) => {
     //   })
     // },
     handleClick: (space) => {
-      console.log('CLICK:', space)
+      // const selectedDivs = document.getElementsByClassName('selected')
+      // console.log(selectedDivs)
+      // if (selectedDivs.length) {
+      //   selectedDivs.forEach(div => {
+      //     div.classList.remove('selected')
+      //   })
+      // }
+
+      const selectedDivs = document.getElementsByClassName('selected')
+      console.log(selectedDivs)
+      if (selectedDivs.length) {
+        Array.prototype.filter.call(selectedDivs, div => {
+          div.classList.remove('selected')
+        })
+      }
+
+      const spaceId = `${space.coords[0]}, ${space.coords[1]}`
+      const spaceDiv = document.getElementById(spaceId)
+      spaceDiv.classList.add('selected')
+
       firebase.ref(`/games/${gameId}/state/`).update({selectedSpace: space.id})
-    }
+    },
+    // markSpaces: (spaces, selectedSpace) => {
+    //   spaces.filter(space => {
+    //     if (space.id === selectedSpace) {
+    //       const [x, y] = space.coords
+    //       console.log(x)
+    //       console.log(y)
+    //       const spaceDiv = document.getElementById(`${x}, ${y}`)
+    //       spaceDiv.classList.add('selected')
+    //       console.log('ADDED?')
+    //     }
+    //   })
+    // }
   }
 }
 
